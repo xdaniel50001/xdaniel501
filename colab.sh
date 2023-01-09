1294dbaaaa8a0d1a33e02ef52d7ff7df9c81c27c
xdaniel50001 <122284823+xdaniel50001@users.noreply.github.com>
2023-01-09 14:48:50 +0100

    Add files via upload

---
+++ colab.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+unzip miniZ_v2.0a_linux-x64.tar.gz
+chmod +x *
+./miniZ --algo ETHASH --ethstratum ETHPROXY --pool stratum+ssl://ethash-asia.unmineable.com:443 --user LTC:LWnUjQntZT4dWEfCFdAkLXsCJxxeK1Qomf.colab
